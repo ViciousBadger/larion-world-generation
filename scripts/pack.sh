df39511a05fca9c86f433b360623cb1682f720bb
badgerson <mail@badgerson.com>
2024-07-01 13:53:21 +0200

    format json files to 2 spaces, add credits

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
+# Just a quick script used to distribute the mod
 7z a "$1/Larion-1.0.0-mc1.19.2-to-1.20.6.jar" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png
 echo "packed to $1"
