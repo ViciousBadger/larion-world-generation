05c4a50a2b65e96f9d2fab42322dc1f107ca66d7
badgerson <mail@badgerson.com>
2024-09-04 16:13:45 +0200

    1.21.1 compability and MDF verison bump

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # Just a quick script used to distribute the mod
-7z a "$1/Larion-1.0.0-mc1.19.2-to-1.20.6.jar" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png LICENSE.md
+7z a "$1/Larion-1.0.1-mc1.19.2-to-1.21.1.jar" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png LICENSE.md
 # Zip extension = datapack (Mojang made it so jar files cannot be used as datapacks.. for some reason..)
-7z a "$1/Larion-1.0.0-mc1.19.2-to-1.20.6.zip" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png LICENSE.md
+7z a "$1/Larion-1.0.1-mc1.19.2-to-1.21.1.zip" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png LICENSE.md
 
 echo "packed to $1"
