99683dd1d7f62a5fc7801ade3379cf0069c6979b
badgerson <mail@badgerson.com>
2024-09-07 19:31:30 +0200

    reduce world height back to vanilla

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 filename='Larion-2.0.0-mc1.19.2-to-1.21.1'
 # Just a quick script used to distribute the mod
-7z a "$1/${filename}.jar" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png LICENSE.md
+7z a "$1/${filename}.jar" data fabric.mod.json pack.mcmeta pack.png LICENSE.md
 # Zip extension = datapack (Mojang made it so jar files cannot be used as datapacks.. for some reason..)
-7z a "$1/${filename}.zip" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png LICENSE.md
+7z a "$1/${filename}.zip" data fabric.mod.json pack.mcmeta pack.png LICENSE.md
 
 echo "packed to $1"
