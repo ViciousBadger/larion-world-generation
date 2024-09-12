d52fb76107fa3e505676f8a1e55f1f0eb7f17f54
badgerson <mail@badgerson.com>
2024-09-12 13:13:58 +0200

    version bump, tweak river more, even more continent variation

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-filename='Larion-3.0.1-mc1.19.2-to-1.21.1'
 # Just a quick script used to distribute the mod
+filename='Larion-3.1.0'
 7z a "$1/${filename}.jar" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png LICENSE.md
 # Zip extension = datapack (Mojang made it so jar files cannot be used as datapacks.. for some reason..)
 7z a "$1/${filename}.zip" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png LICENSE.md
