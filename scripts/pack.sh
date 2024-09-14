012c2d7f3d5d830ff2abc5377208ed617e27ac5a
badgerson <mail@badgerson.com>
2024-09-14 23:30:27 +0200

    bump version

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # Just a quick script used to distribute the mod
-filename='Larion-dev'
+filename='Larion-3.2.0'
 7z a "$1/${filename}.jar" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png LICENSE.md
 # Zip extension = datapack (Mojang made it so jar files cannot be used as datapacks.. for some reason..)
 7z a "$1/${filename}.zip" data 1-20-5-overlay fabric.mod.json pack.mcmeta pack.png LICENSE.md
