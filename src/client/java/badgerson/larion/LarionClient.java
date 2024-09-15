b48e17d6f26963e8fae7fbe4cd0c8bf2a262d16e
badgerson <mail@badgerson.com>
2024-09-15 18:52:41 +0200

    move things

--- src/client/java/badgerson/larion/LarionClient.java
+++ src/client/java/badgerson/larion/LarionClient.java
@@ -2,7 +2,7 @@ package badgerson.larion;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class LarionWorldGenerationClient implements ClientModInitializer {
+public class LarionClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/badgerson/larion/Larion.java
+++ src/main/java/badgerson/larion/Larion.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LarionWorldGeneration implements ModInitializer {
+public class Larion implements ModInitializer {
 	public static final String MOD_ID = "larion";
 
 	// This logger is used to write text to the console and the log file.
