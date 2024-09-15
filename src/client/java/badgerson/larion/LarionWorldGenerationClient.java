f23c79f05852c8a3e40e117e5b6339ce9f5fa1e4
badgerson <mail@badgerson.com>
2024-09-15 18:40:35 +0200

    set up fabric modding environment

---
+++ src/client/java/badgerson/larion/LarionWorldGenerationClient.java
@@ -0,0 +1,10 @@
+package badgerson.larion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LarionWorldGenerationClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
