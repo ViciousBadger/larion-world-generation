6c366139ae4f53d95c1ac97c84a617aca058e90f
badgerson <mail@badgerson.com>
2024-09-17 21:40:02 +0200

    experiments, remove most caves for now

--- src/main/java/com/badgerson/larion/Larion.java
+++ src/main/java/com/badgerson/larion/Larion.java
@@ -11,6 +11,8 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+import net.minecraft.world.gen.carver.CaveCarver;
+
 public class Larion implements ModInitializer {
     public static final String MOD_ID = "larion";
 
