3c932e48b6c509bc9514a9aafa4c15caccf6f0f6
badgerson <mail@badgerson.com>
2024-09-19 18:34:24 +0200

    move world so lowest point is -96

--- src/main/java/com/badgerson/larion/mixin/NoiseChunkGeneratorMixin.java
+++ src/main/java/com/badgerson/larion/mixin/NoiseChunkGeneratorMixin.java
@@ -15,10 +15,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class NoiseChunkGeneratorMixin {
     @Inject(method = "createFluidLevelSampler", at = @At("HEAD"), cancellable = true)
     private static void createFluidLevelSampler(ChunkGeneratorSettings settings, CallbackInfoReturnable<AquiferSampler.FluidLevelSampler> ci) {
-		AquiferSampler.FluidLevel fluidLevel = new AquiferSampler.FluidLevel(-118, Blocks.LAVA.getDefaultState());
+		AquiferSampler.FluidLevel fluidLevel = new AquiferSampler.FluidLevel(-86, Blocks.LAVA.getDefaultState());
 		int i = settings.seaLevel();
 		AquiferSampler.FluidLevel fluidLevel2 = new AquiferSampler.FluidLevel(i, settings.defaultFluid());
 		// AquiferSampler.FluidLevel fluidLevel3 = new AquiferSampler.FluidLevel(DimensionType.MIN_HEIGHT * 2, Blocks.AIR.getDefaultState());
-        ci.setReturnValue((x, y, z) -> y < Math.min(-118, i) ? fluidLevel : fluidLevel2);
+        ci.setReturnValue((x, y, z) -> y < Math.min(-86, i) ? fluidLevel : fluidLevel2);
     }
 }
