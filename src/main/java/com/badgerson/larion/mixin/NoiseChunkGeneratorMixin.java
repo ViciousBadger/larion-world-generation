fba948834da7f0c2a9a8e70db947a6f720e01cf8
badgerson <mail@badgerson.com>
2024-09-21 12:21:58 +0200

    make bedrock lava height dynamic

--- src/main/java/com/badgerson/larion/mixin/NoiseChunkGeneratorMixin.java
+++ src/main/java/com/badgerson/larion/mixin/NoiseChunkGeneratorMixin.java
@@ -14,10 +14,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class NoiseChunkGeneratorMixin {
     @Inject(method = "createFluidLevelSampler", at = @At("HEAD"), cancellable = true)
     private static void createFluidLevelSampler(ChunkGeneratorSettings settings, CallbackInfoReturnable<AquiferSampler.FluidLevelSampler> ci) {
-		AquiferSampler.FluidLevel fluidLevel = new AquiferSampler.FluidLevel(-118, Blocks.LAVA.getDefaultState());
+        // This mixin modifies the aquifer generator so that the "bedrock lava
+        // level" (in vanilla hardcoded to -54) is instead determined by the world's
+        // minimum Y level, allowing for deeper caves without them being flooded.
+        // Ideally the lava sea level would be a field in the noise settings
+        // like normal sea level, but this is easier to implement
+        var lavaSeaLevel = settings.generationShapeConfig().minimumY() + 10;
+		AquiferSampler.FluidLevel fluidLevel = new AquiferSampler.FluidLevel(lavaSeaLevel, Blocks.LAVA.getDefaultState());
 		int i = settings.seaLevel();
 		AquiferSampler.FluidLevel fluidLevel2 = new AquiferSampler.FluidLevel(i, settings.defaultFluid());
 		// AquiferSampler.FluidLevel fluidLevel3 = new AquiferSampler.FluidLevel(DimensionType.MIN_HEIGHT * 2, Blocks.AIR.getDefaultState());
-        ci.setReturnValue((x, y, z) -> y < Math.min(-118, i) ? fluidLevel : fluidLevel2);
+        ci.setReturnValue((x, y, z) -> y < Math.min(lavaSeaLevel, i) ? fluidLevel : fluidLevel2);
     }
 }
