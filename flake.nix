0469e5c86aa7e3e2bfceadb7dcd1ebfd5201c2e8
badgerson <mail@badgerson.com>
2025-02-15 22:08:19 +0100

    fail

--- flake.nix
+++ flake.nix
@@ -19,14 +19,19 @@
       formatter = pkgs.alejandra;
 
       devShells.default = pkgs.mkShell rec {
-        buildInputs = with pkgs; let
-          jdkVersion = jdk17;
-        in [
-          jdkVersion
+        buildInputs = with pkgs; [
+          jdk17
           libGL
-          (jdt-language-server.override {jdk = jdkVersion;})
+          (jdt-language-server.override {jdk = jdk17;})
+          (gradle_8.override
+            {
+              java = jdk17;
+              javaToolchains = [jdk17 jdk21];
+            })
         ];
         LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath buildInputs}";
+        # JAVA_17_HOME = "${pkgs.jdk17}/lib/openjdk";
+        # JAVA_21_HOME = "${pkgs.jdk21}/lib/openjdk";
         # LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib";
       };
     });
